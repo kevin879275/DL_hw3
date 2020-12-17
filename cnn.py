c9874289039d8fc3c9dda276dea9b940f0b26c25
kevin879275 <gevin879275@gmail.com>
2020-12-18 01:55:10 +0800

    update

--- cnn.py
+++ cnn.py
@@ -3,6 +3,7 @@ import numpy as np
 in_channels = 3
 out_channels = 2
 
+BATCH_SIZE = 2
 padding = 1
 strides = 1
 
@@ -10,11 +11,11 @@ img_H = 5
 img_W = 5
 kernel_size = (3, 3)
 
-img = np.random.randint(4, size=(in_channels, img_H, img_W))
+img = np.random.randint(4, size=(BATCH_SIZE, in_channels, img_H, img_W))
 
 print("img = \n", img)
 if padding > 0:
-    img = np.pad(array=img, pad_width=((0, 0), (padding, padding),
+    img = np.pad(array=img, pad_width=((0, 0), (0, 0), (padding, padding),
                                        (padding, padding)), mode='constant', constant_values=0)
 weights = np.random.randint(
     4, size=(out_channels, in_channels, kernel_size[0], kernel_size[1]))
@@ -23,7 +24,7 @@ output_shape_H = int(
     ((img_H - kernel_size[0] + 2*padding) / strides) + 1)
 output_shape_W = int(
     ((img_W - kernel_size[1] + 2*padding) / strides) + 1)
-output = np.zeros((out_channels, output_shape_H, output_shape_W))
+output = np.zeros((BATCH_SIZE, out_channels, output_shape_H, output_shape_W))
 
 print("padding_img = \n", img)
 print("weights = \n", weights)
@@ -31,9 +32,10 @@ print("weights = \n", weights)
 for channel in range(out_channels):
     for H in range(output_shape_H):
         for W in range(output_shape_W):
-            dot = np.multiply(img[:, H * strides: H*strides + kernel_size[0],
+            dot = np.multiply(img[:, :, H * strides: H*strides + kernel_size[0],
                                   W*strides:W*strides+kernel_size[1]], weights[channel])
-            output[channel, H, W] = np.sum(dot)
+            s = np.sum(dot, axis=(1, 2, 3))
+            output[:, channel, H, W] = s
 
 
 print("output : \n", output)
